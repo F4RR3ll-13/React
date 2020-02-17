dfda1dc737bc13b04bc2cefd941180f0deda7d5e
F4RR3ll-13 <den_klassnyi@mail.ru>
2020-02-17 17:21:58 +0300

    first steps

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Happy Hacking
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/F4RR3ll-13"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          F4RR3LL
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
